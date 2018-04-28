3ab9f3f0af7c2c69ee14b44383b268d60aaa815e
Daniel Loman <dloman@dons.usfca.edu>
2018-04-28 17:24:54 -0400

    04/28/18

---
+++ PlayerStatsAgg.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 11:03:47 2018
+
+@author: DanLo1108
+"""
+
+import numpy as np
+import pandas as pd
+import sqlalchemy as sa 
+import os
+import yaml
+
+
+#Get credentials stored in sql.yaml file (saved in root directory)
+if os.path.isfile('/sql.yaml'):
+    with open("/sql.yaml", 'r') as stream:
+        data_loaded = yaml.load(stream)
+        
+        #domain=data_loaded['SQL_DEV']['domain']
+        user=data_loaded['BBALL_STATS']['user']
+        password=data_loaded['BBALL_STATS']['password']
+        endpoint=data_loaded['BBALL_STATS']['endpoint']
+        port=data_loaded['BBALL_STATS']['port']
+        database=data_loaded['BBALL_STATS']['database']
+        
+db_string = "postgres://{0}:{1}@{2}:{3}/{4}".format(user,password,endpoint,port,database)
+engine=sa.create_engine(db_string)
+
+
+player_stats_agg_query='''
+
+select 
+    pb."Player"
+    ,pb."PlayerID"
+    ,pb."Team"
+    ,gs."Season"
+    ,gs."GameType"
+    ,count(*) "GP"
+    ,sum(pb."MP") "MP"
+    ,avg(pb."PTS") "PPG"
+    ,avg(pb."FGM") "FGM"
+    ,avg(pb."FGA") "FGA"
+    ,sum(pb."FGM")*1.0/sum(pb."FGA") "FG_Pct"
+    ,avg(pb."3PTM") "FG3M"
+    ,avg(pb."3PTA") "FG3A"
+    ,case when sum(pb."3PTA") > 0 then sum(pb."3PTM")*1.0/sum(pb."3PTA") else Null end "FG3_Pct"
+    ,avg(pb."FTM") "FTM"
+    ,avg(pb."FTA") "FTA"
+    ,sum(pb."FTM")*1.0/sum(pb."FTA") "FT_Pct"
+    ,avg(pb."OREB") "OREB"
+    ,avg(pb."DREB") "DREB"
+    ,avg(pb."REB") "REB"
+    ,avg(pb."AST") "AST"
+    ,avg(pb."STL") "STL"
+    ,avg(pb."BLK") "BLK"
+    ,avg(pb."TOV") "TOV"
+    ,avg(pb."PF") "PF"
+    ,sum(pb."PlusMinus") "RawPlusMinus"
+    ,sum(pb."PTS")*36.0/sum(pb."MP") "PTS_36"
+    ,sum(pb."FGM")*36.0/sum(pb."MP") "FGM_36"
+    ,sum(pb."FGA")*36.0/sum(pb."MP") "FGA_36"
+    ,sum(pb."3PTM")*36.0/sum(pb."MP") "FG3M_36"
+    ,sum(pb."3PTA")*36.0/sum(pb."MP") "FG3A_36"
+    ,sum(pb."FTM")*36.0/sum(pb."MP") "FTM_36"
+    ,sum(pb."FTA")*36.0/sum(pb."MP") "FTA_36"
+    ,sum(pb."OREB")*36.0/sum(pb."MP") "OREB_36"
+    ,sum(pb."DREB")*36.0/sum(pb."MP") "DREB_36"
+    ,sum(pb."REB")*36.0/sum(pb."MP") "REB_36"
+    ,sum(pb."AST")*36.0/sum(pb."MP") "AST_36"
+    ,sum(pb."STL")*36.0/sum(pb."MP") "STL_36"
+    ,sum(pb."BLK")*36.0/sum(pb."MP") "BLK_36"
+    ,sum(pb."TOV")*36.0/sum(pb."MP") "TOV_36"
+    ,sum(pb."PF")*36.0/sum(pb."MP") "PF_36"
+    ,(sum(pb."FGM")+sum(pb."3PTM")*.5)/sum(pb."FGA") "eFG_Pct"
+    ,sum(pb."PTS")/(2*(sum(pb."FGA")+0.44*sum(pb."FTA"))) "TS_Pct"
+    ,sum(pb."3PTA")*1.0/sum(pb."FGA") "FG3_Rate"
+    ,sum(pb."FTA")*1.0/sum(pb."FGA") "FT_Rate"
+    ,(sum(pb."OREB")*(tsa."MP"/5.0))/(sum(pb."MP")*(tsa."OREB"*tsa."GP"+tsa."DREB_opp"*tsa."GP")) "OREB_Pct"
+    ,(sum(pb."DREB")*(tsa."MP"/5.0))/(sum(pb."MP")*(tsa."DREB"*tsa."GP"+tsa."OREB_opp"*tsa."GP")) "DREB_Pct"
+    ,(sum(pb."REB")*(tsa."MP"/5.0))/(sum(pb."MP")*(tsa."REB"*tsa."GP"+tsa."REB_opp"*tsa."GP")) "REB_Pct"
+    ,sum(pb."AST")*1.0/(((sum(pb."MP")/(tsa."MP"/5))*tsa."FGM"*tsa."GP")-sum(pb."FGM")) "AST_Pct"
+    ,sum(pb."STL")*1.0/((sum(pb."MP")/(tsa."MP"/5))*tsa."Poss") "STL_Pct"
+    ,sum(pb."BLK")*1.0/((sum(pb."MP")/(tsa."MP"/5))*(tsa."FGA_opp"*tsa."GP"-tsa."FG3A_opp"*tsa."GP")) "BLK_Pct"
+    ,sum(pb."TOV")*1.0/(sum(pb."FGA")+0.44*sum(pb."FTA")+sum(pb."TOV")) "TOV_Pct"
+    ,(sum(pb."FGA")+0.44*sum(pb."FTA")+sum(pb."TOV"))/((sum(pb."MP")/(tsa."MP"/5))*(tsa."FGA"*tsa."GP"+0.44*tsa."FTA"*tsa."GP"+tsa."TOV"*tsa."GP")) "USG_Pct"    
+from 
+    nba.player_boxscores pb 
+join
+    nba.game_summaries gs on pb."GameID"=gs."GameID"
+left join
+	nba.team_stats_agg tsa on pb."Team"=tsa."Team" and gs."GameType"=tsa."GameType" and gs."Season"=tsa."Season"
+where 1=1
+    and pb."DNP_Reason" is null
+group by
+    pb."Player"
+    ,pb."PlayerID"
+    ,pb."Team"
+    ,gs."Season"
+	,gs."GameType"
+	,tsa."MP"
+	,tsa."GP"
+	,tsa."DREB"
+	,tsa."OREB_opp"
+	,tsa."OREB"
+	,tsa."DREB_opp"
+	,tsa."REB"
+	,tsa."REB_opp"
+	,tsa."FGM"
+	,tsa."Poss"
+	,tsa."FGA_opp"
+	,tsa."FG3A_opp"
+	,tsa."FGA"
+	,tsa."FTA"
+	,tsa."TOV"
+having
+    sum(pb."FGA") > 0 and sum(pb."FTA") > 0 and sum(pb."TOV") > 0
+
+'''
+
+player_stats_agg=pd.read_sql(player_stats_agg_query,engine)
+
+
+player_stats_agg.to_sql('player_stats_agg',con=engine,schema='nba',index=False,if_exists='replace')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TeamStatsAgg.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 28 17:21:21 2018
+
+@author: DanLo1108
+"""
+
+
+import numpy as np
+import pandas as pd
+import sqlalchemy as sa 
+import os
+import yaml
+
+
+#Get credentials stored in sql.yaml file (saved in root directory)
+if os.path.isfile('/sql.yaml'):
+    with open("/sql.yaml", 'r') as stream:
+        data_loaded = yaml.load(stream)
+        
+        #domain=data_loaded['SQL_DEV']['domain']
+        user=data_loaded['BBALL_STATS']['user']
+        password=data_loaded['BBALL_STATS']['password']
+        endpoint=data_loaded['BBALL_STATS']['endpoint']
+        port=data_loaded['BBALL_STATS']['port']
+        database=data_loaded['BBALL_STATS']['database']
+        
+db_string = "postgres://{0}:{1}@{2}:{3}/{4}".format(user,password,endpoint,port,database)
+engine=sa.create_engine(db_string)
+
+
+team_stats_agg_query='''
+with possessions as (
+	select
+		tb."Team"
+		,gs."Season"
+		,gs."GameType"
+		,.5*((sum(tb."FGA")+0.4*sum(tb."FTA")-1.07*(sum(tb."OREB")*1.0/(sum(tb."OREB")+sum(tb."DREB")))*(sum(tb."FGA")-sum(tb."FGM"))+sum(tb."TOV"))+(sum(tb."FGA_opp")+0.4*sum(tb."FTA_opp")-1.07*(sum(tb."OREB_opp")*1.0/(sum(tb."OREB_opp")+sum(tb."DREB_opp"))) * (sum(tb."FGA_opp")-sum(tb."FGM_opp"))+sum(tb."TOV_opp"))) "Poss"
+	from
+		nba.team_boxscores tb
+	join
+		nba.game_summaries gs on tb."GameID"=gs."GameID" and gs."Status"='Final'
+	group by
+		tb."Team"
+		,gs."Season"
+		,gs."GameType"
+)
+
+select
+	tb."Team"
+	,max(case 
+		when tb."Team"=gs."HomeTeamAbbr" then gs."HomeTeamID" 
+		when tb."Team"=gs."AwayTeamAbbr" then gs."AwayTeamID" else Null end) "TeamID"
+	,gs."Season"
+	,gs."GameType"
+	,count(*) "GP"
+	,sum(case when tb."PTS" > tb."PTS_opp" then 1 else 0 end) "Wins"
+	,sum(case when tb."PTS" < tb."PTS_opp" then 1 else 0 end) "Losses"
+	,avg(case when tb."PTS" > tb."PTS_opp" then 1.0 else 0 end) "WinPct"
+	,avg(tb."FGM"*1.0) "FGM"
+	,avg(tb."FGA"*1.0) "FGA"
+	,sum(tb."FGM")*1.0/sum(tb."FGA") "FG_Pct"
+	,avg(tb."3PTM"*1.0) "FG3M"
+	,avg(tb."3PTA"*1.0) "FG3A"
+	,sum(tb."3PTM")*1.0/sum(tb."3PTA") "FG3_Pct"
+	,avg(tb."FTM"*1.0) "FTM"
+	,avg(tb."FTA"*1.0) "FTA"
+	,sum(tb."FTM")*1.0/sum(tb."FTA") "FT_Pct"
+	,avg(tb."PTS") "PTS"
+	,avg(tb."OREB")+avg(tb."DREB") "REB"
+	,avg(tb."OREB") "OREB"
+	,avg(tb."DREB") "DREB"
+	,avg(tb."AST") "AST"
+	,avg(tb."STL") "STL"
+	,avg(tb."BLK") "BLK"
+	,avg(tb."TOV") "TOV"
+	,avg(tb."PtsOffTOV") "PtsOffTOV"
+	,avg(tb."FstBrkPts") "FstBrkPts"
+	,avg(tb."PtsInPnt") "PtsInPnt"
+	,avg(tb."PF") "PF"
+	,avg(tb."TechF") "TechF"
+	,avg(tb."FlagF") "FlagF"
+	,avg(tb."FGM_opp"*1.0) "FGM_opp"
+	,avg(tb."FGA_opp"*1.0) "FGA_opp"
+	,sum(tb."FGM_opp")*1.0/sum(tb."FGA_opp") "FG_Pct_opp"
+	,avg(tb."3PTM_opp"*1.0) "FG3M_opp"
+	,avg(tb."3PTA_opp"*1.0) "FG3A_opp"
+	,sum(tb."3PTM_opp")*1.0/sum(tb."3PTA_opp") "FG3_Pct_opp"
+	,avg(tb."FTM_opp"*1.0) "FTM_opp"
+	,avg(tb."FTA_opp"*1.0) "FTA_opp"
+	,sum(tb."FTM_opp")*1.0/sum(tb."FTA_opp") "FT_Pct_opp"
+	,avg(tb."PTS_opp") "PTS_opp"
+	,avg(tb."OREB_opp")+avg(tb."DREB_opp") "REB_opp"
+	,avg(tb."OREB_opp") "OREB_opp"
+	,avg(tb."DREB_opp") "DREB_opp"
+	,avg(tb."AST_opp") "AST_opp"
+	,avg(tb."STL_opp") "STL_opp"
+	,avg(tb."BLK_opp") "BLK_opp"
+	,avg(tb."TOV_opp") "TOV_opp"
+	,avg(tb."PtsOffTOV_opp") "PtsOffTOV_opp"
+	,avg(tb."FstBrkPts_opp") "FstBrkPts_opp"
+	,avg(tb."PtsInPnt_opp") "PtsInPnt_opp"
+	,avg("PF_opp") "PF_opp"
+	,avg("TechF_opp") "TechF_opp"
+	,avg("FlagF_opp") "FlagF_opp"
+	--Get 4 factors + ratings
+	,sum(pb."MP") "MP"
+	,p."Poss"
+	,48*((p."Poss"*2)/(2*(sum(pb."MP")/5.0))) "Pace"
+	,sum(tb."PTS")/p."Poss"*100 "OffRTG"
+	,sum(tb."PTS_opp")*100.0/p."Poss" "DefRTG"
+	,(sum(tb."PTS")-sum(tb."PTS_opp"))/p."Poss"*100 "NetRTG"
+	,sum(tb."3PTA")*1.0/sum(tb."FGA") "FG3_Rate"
+	,sum(tb."FTA")*1.0/sum(tb."FGA") "FT_Rate"
+	,(sum(tb."FGM")+.5*sum(tb."3PTM"))/sum(tb."FGA") "eFG_Pct"
+	,sum(tb."TOV")/(sum(tb."FGA")+.44*sum(tb."FTA")+sum(tb."TOV")) "TOV_Pct"
+	,sum(tb."OREB")*1.0/(sum(tb."OREB")+sum(tb."DREB_opp")) "OREB_Pct"
+	,sum(tb."FTM")*1.0/sum(tb."FGA") "FF_FT_Rate"
+	,sum(tb."3PTA_opp")*1.0/sum(tb."FGA_opp") "FG3_Rate_opp"
+	,sum(tb."FTA_opp")*1.0/sum(tb."FGA_opp") "FT_Rate_opp"
+	,(sum(tb."FGM_opp")+.5*sum(tb."3PTM_opp"))/sum(tb."FGA_opp") "eFG_Pct_opp"
+	,sum(tb."TOV_opp")/(sum(tb."FGA_opp")+.44*sum(tb."FTA_opp")+sum(tb."TOV_opp")) "TOV_Pct_opp"
+	,sum(tb."OREB_opp")*1.0/(sum(tb."OREB_opp")+sum(tb."DREB")) "OREB_Pct_opp"
+	,sum(tb."FTM_opp")*1.0/sum(tb."FGA_opp") "FF_FT_Rate_opp"
+from
+	nba.team_boxscores tb
+join
+	nba.game_summaries gs on tb."GameID"=gs."GameID" and gs."Status"='Final'
+left join
+	(select "GameID","Team",sum(cast("MP" as float)) "MP" from nba.player_boxscores group by "GameID","Team") pb on tb."GameID"=pb."GameID" and tb."Team"=pb."Team"
+join
+	possessions p on tb."Team"=p."Team" and gs."Season"=p."Season" and gs."GameType"=p."GameType"
+group by
+	tb."Team"
+	,gs."Season"
+	,gs."GameType"
+	,p."Poss"
+'''
+
+
+team_stats_agg=pd.read_sql(team_stats_agg_query,engine)
+
+
+team_stats_agg.to_sql('team_stats_agg',con=engine,schema='nba',index=False,if_exists='replace')
