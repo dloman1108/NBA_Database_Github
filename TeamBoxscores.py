d6199075b835b9c643769977ea3c0cf9c2668e6c
Daniel Loman <dh08loma@Daniels-MacBook-Pro.local>
2019-11-12 17:47:05 -0500

    Update Data Types

--- TeamBoxscores.py
+++ TeamBoxscores.py
@@ -9,14 +9,13 @@ Created on Tue Nov 29 12:13:57 2016
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
-import urllib2
 import re
 import os
 import yaml
 import string as st
 
 import sqlalchemy as sa
-
+from urllib.request import urlopen
 
 def get_made(x,var):
     x_var=x[var]
@@ -45,8 +44,7 @@ def append_team_boxscores(game_id,engine):
     
     url='http://www.espn.com/nba/matchup?gameId='+game_id
     
-    request=urllib2.Request(url)
-    page = urllib2.urlopen(request)
+    page = urlopen(url)
     
     
     content=page.read()
@@ -73,16 +71,24 @@ def append_team_boxscores(game_id,engine):
     tm1_pts=results_head_split[0][-1]
     tm2_pts=results_head_split[1][-1]
     
-    team_stats_df=pd.DataFrame([([tm1,tm1_pts]+results_body[1::3]),
-                  ([tm2,tm2_pts]+results_body[2::3])],
-                columns=['Team','PTS','FG','FG_Pct','FG3','FG3_Pct',
-                         'FT','FT_Pct','REB','OREB','DREB','AST','STL',
-                         'BLK','TOV','PtsOffTOV','FstBrkPts','PtsInPnt',
-                         'PF','TechF','FlagF'])
+    if len(results_body) == 60:
+        team_stats_df=pd.DataFrame([([tm1,tm1_pts]+results_body[:-3][1::3]),
+                      ([tm2,tm2_pts]+results_body[:-3][2::3])],
+                    columns=['Team','PTS','FG','FG_Pct','FG3','FG3_Pct',
+                             'FT','FT_Pct','REB','OREB','DREB','AST','STL',
+                             'BLK','TOV','PtsOffTOV','FstBrkPts','PtsInPnt',
+                             'PF','TechF','FlagF'])
+    else:
+        team_stats_df=pd.DataFrame([([tm1,tm1_pts]+results_body[1::3]),
+                      ([tm2,tm2_pts]+results_body[2::3])],
+                    columns=['Team','PTS','FG','FG_Pct','FG3','FG3_Pct',
+                             'FT','FT_Pct','REB','OREB','DREB','AST','STL',
+                             'BLK','TOV','PtsOffTOV','FstBrkPts','PtsInPnt',
+                             'PF','TechF','FlagF'])
                          
     for col in team_stats_df:
         try:
-            team_stats_df[col]=map(lambda x: int(x), team_stats_df[col])
+            team_stats_df[col]=list(map(lambda x: int(x), team_stats_df[col]))
         except:
             continue
     
@@ -108,8 +114,83 @@ def append_team_boxscores(game_id,engine):
     team_stats_df['DRTG']=team_stats_df.PTS_opp*100/team_stats_df.Poss*100
     team_stats_df['NetRTG']=(team_stats_df.PTS-team_stats_df.PTS_opp)/team_stats_df.Poss
     
-    team_stats_df.to_sql('team_boxscores',con=engine,schema='nba',index=False,if_exists='append')
-
+    column_order=['GameID', 'Team', 'PTS', 'FG', 'FGM', 'FGA', 'FG_Pct', 'FG3', 'FG3_Pct', 
+                  '3PTM','3PTA', 'FT', 'FTM', 'FTA', 'FT_Pct', 'OREB', 'DREB', 'REB',
+                  'AST', 'STL', 'BLK', 'TOV', 'PtsOffTOV', 'FstBrkPts','PtsInPnt', 'PF', 
+                  'TechF', 'FlagF', 'OppTeam', 'PTS_opp', 'FG_opp', 'FGM_opp', 'FGA_opp',
+                  'FG_Pct_opp', 'FG3_opp', '3PTM_opp', '3PTA_opp', 'FG3_Pct_opp', 'FT_opp', 
+                  'FTM_opp', 'FTA_opp', 'FT_Pct_opp', 'OREB_opp', 'DREB_opp', 'REB_opp',
+                  'AST_opp', 'STL_opp', 'BLK_opp', 'TOV_opp', 'PtsOffTOV_opp',
+                  'FstBrkPts_opp', 'PtsInPnt_opp', 'PF_opp', 'TechF_opp', 'FlagF_opp',
+                  'Poss', 'Poss_opp', 'ORTG', 'DRTG', 'NetRTG']
+    
+    team_stats_df[column_order].to_sql('team_boxscores',
+                         con=engine,
+                         schema='nba',
+                         index=False,
+                         if_exists='append',
+                         dtype={'GameID': sa.types.INTEGER(),
+                                'Team': sa.types.VARCHAR(length=255),
+                                'PTS': sa.types.INTEGER(),
+                                'FG': sa.types.VARCHAR(length=255),
+                                'FGM': sa.types.INTEGER(),
+                                'FGA': sa.types.INTEGER(),
+                                'FG_Pct': sa.types.FLOAT(),
+                                '3PT': sa.types.VARCHAR(length=255),
+                                '3PTM': sa.types.INTEGER(),
+                                '3PTA': sa.types.INTEGER(),
+                                'FG3_Pct': sa.types.FLOAT(),
+                                'FT': sa.types.VARCHAR(length=255),
+                                'FTM': sa.types.INTEGER(),
+                                'FTA': sa.types.INTEGER(),
+                                'FT_Pct': sa.types.FLOAT(),
+                                'OREB': sa.types.INTEGER(),
+                                'DREB': sa.types.INTEGER(),
+                                'REB': sa.types.INTEGER(),
+                                'AST': sa.types.INTEGER(),
+                                'STL': sa.types.INTEGER(),
+                                'BLK': sa.types.INTEGER(),
+                                'TOV': sa.types.INTEGER(),
+                                'PtsOffTOV': sa.types.INTEGER(),
+                                'FstBrkPts': sa.types.INTEGER(),
+                                'PtsInPnt': sa.types.INTEGER(),
+                                'PF': sa.types.INTEGER(),
+                                'TechF': sa.types.INTEGER(),
+                                'FlagF': sa.types.INTEGER(),
+                                
+                                'OppTeam': sa.types.VARCHAR(length=255),
+                                'PTS_opp': sa.types.INTEGER(),
+                                'FG_opp': sa.types.VARCHAR(length=255),
+                                'FGM_opp': sa.types.INTEGER(),
+                                'FGA_opp': sa.types.INTEGER(),
+                                'FG_Pct_opp': sa.types.FLOAT(),
+                                '3PT_opp': sa.types.VARCHAR(length=255),
+                                '3PTM_opp': sa.types.INTEGER(),
+                                '3PTA_opp': sa.types.INTEGER(),
+                                'FG3_Pct_opp': sa.types.FLOAT(),
+                                'FT_opp': sa.types.VARCHAR(length=255),
+                                'FTM_opp': sa.types.INTEGER(),
+                                'FTA_opp': sa.types.INTEGER(),
+                                'FT_Pct_opp': sa.types.FLOAT(),
+                                'OREB_opp': sa.types.INTEGER(),
+                                'DREB_opp': sa.types.INTEGER(),
+                                'REB_opp': sa.types.INTEGER(),
+                                'AST_opp': sa.types.INTEGER(),
+                                'STL_opp': sa.types.INTEGER(),
+                                'BLK_opp': sa.types.INTEGER(),
+                                'TOV_opp': sa.types.INTEGER(),
+                                'PtsOffTOV_opp': sa.types.INTEGER(),
+                                'FstBrkPts_opp': sa.types.INTEGER(),
+                                'PtsInPnt_opp': sa.types.INTEGER(),
+                                'PF_opp': sa.types.INTEGER(),
+                                'TechF_opp': sa.types.INTEGER(),
+                                'FlagF_opp': sa.types.INTEGER(),
+                                
+                                'Poss': sa.types.FLOAT(),
+                                'Poss_opp': sa.types.FLOAT(),
+                                'ORTG': sa.types.FLOAT(),
+                                'DRTG': sa.types.FLOAT(),
+                                'NetRTG': sa.types.FLOAT()})
         
     
 def get_engine():
@@ -161,25 +242,25 @@ def update_team_boxscores(engine,game_id_list):
     for game_id in game_id_list:
         
         if np.mod(cnt,2000)==0:
-            print 'CHECK: ',cnt,len(bad_gameids)
+            print('CHECK: ',cnt,len(bad_gameids))
     
         try:
             append_team_boxscores(game_id,engine)
             cnt+=1
             if np.mod(cnt,100)==0:
-                print str(round(float(cnt*100.0/len(game_ids)),2))+'%'
+                print(str(round(float(cnt*100.0/len(game_ids)),2))+'%')
             
         except:
             bad_gameids.append(game_id)
             cnt+=1
             if np.mod(cnt,100) == 0:
-                print str(round(float(cnt*100.0/len(game_ids)),2))+'%' 
+                print(str(round(float(cnt*100.0/len(game_ids)),2))+'%')
             continue
         
         
 def main():
     engine=get_engine()
-    game_ids=get_dates(engine)
+    game_ids=get_gameids(engine)
     update_team_boxscores(engine,game_ids)
     
     
